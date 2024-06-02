de9248e9f61b59bd32a6c48431f7b99bb3a7899b
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-02 23:59:21 +0000

    Creating Catch-All Routes
    [year ] => [[...filter]] creates conflict between @archives page.js & [[...filter]]  page.js

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -2,8 +2,8 @@ import NewsList from "@/app/components/news-list";
 import { getNewsForYear } from "@/lib/news";
 
 export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-
+  const filter = params.filter;
+  console.log(filter);
   const news = getNewsForYear(newsYear);
 
   return <NewsList news={news} />;
